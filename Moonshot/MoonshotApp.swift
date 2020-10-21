b5b656ce1563688dd8fd0e89cf5e47a181c04784
Eric Liu <ericliu4943@gmail.com>
2020-10-21 15:12:45 -0700

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Eric Liu on 10/21/20.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
